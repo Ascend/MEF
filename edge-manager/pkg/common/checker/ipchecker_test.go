bc58c0da7c51a6b8c6f85ea4d656efc7636d3514
developer <developer@com>
2022-11-15 11:34:07 +0800

    Match-id-dc264fa57dc7478c0238be8a7398bc83068345dc

--- edge-manager/pkg/common/checker/ipchecker_test.go
+++ edge-manager/pkg/common/checker/ipchecker_test.go
@@ -1,6 +1,7 @@
 package checker
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -9,7 +10,7 @@ func TestIsIpValid(t *testing.T) {
 	ipTestCase := []struct {
 		ip        string
 		expResult bool
-		errMsg    string
+		errMsg    error
 	}{
 		{
 			ip:        "11.111.11.1",
@@ -24,27 +25,28 @@ func TestIsIpValid(t *testing.T) {
 		{
 			ip:        "255.255.255.255",
 			expResult: false,
-			errMsg:    "IP can't be a broadcast address",
+			errMsg:    fmt.Errorf("IP can't be a broadcast address"),
 		},
 		{
 			ip:        "0.0.0.0",
 			expResult: false,
-			errMsg:    "IP can't be an all zeros address",
+			errMsg:    fmt.Errorf("IP can't be an all zeros address"),
 		},
 		{
 			ip:        "::",
 			expResult: false,
-			errMsg:    "IP can't be an all zeros address",
+			errMsg:    fmt.Errorf("IP can't be an all zeros address"),
 		},
 		{
 			ip:        "224.1.1.1",
 			expResult: false,
-			errMsg:    "IP can't be a multicast address",
+			errMsg:    fmt.Errorf("IP can't be a multicast address"),
 		},
 	}
 
 	for index := range ipTestCase {
-		valid, _ := IsIpValid(ipTestCase[index].ip)
+		valid, err := IsIpValid(ipTestCase[index].ip)
 		assert.Equal(t, valid, ipTestCase[index].expResult)
+		assert.Equal(t, err, ipTestCase[index].errMsg)
 	}
 }
