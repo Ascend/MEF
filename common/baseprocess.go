6d5144b24dfe521530594085bb5f6ce5a8b5497b
developer <developer@com>
2023-01-06 14:18:52 +0800

    Match-id-e3a4d4eb52bc1732cf74cf7c21f33572930de2ba

--- common/baseprocess.go
+++ common/baseprocess.go
@@ -13,7 +13,6 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/gin-gonic/gin"
 	"huawei.com/mindx/common/hwlog"
 	"huawei.com/mindxedge/base/modulemanager"
 	"huawei.com/mindxedge/base/modulemanager/model"
@@ -85,22 +84,6 @@ func ParamConvert(input interface{}, reqType interface{}) error {
 		return errors.New("param decode error")
 	}
 	return nil
-}
-
-// BindUriWithJSON convert uri to key-value string dict
-func BindUriWithJSON(c *gin.Context) ([]byte, error) {
-	if c == nil {
-		return nil, errors.New("gin Context can't be nil")
-	}
-	obj := make(map[string][]string, len(c.Params))
-	for _, v := range c.Params {
-		params, ok := obj[v.Key]
-		if !ok {
-			params = make([]string, 0, 1)
-		}
-		obj[v.Key] = append(params, v.Value)
-	}
-	return json.Marshal(obj)
 }
 
 // GetEdgeMgrWorkPath gets edge-manager work path
