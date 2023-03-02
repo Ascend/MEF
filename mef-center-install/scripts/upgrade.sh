52219b2905bd365c247e680f92cdca65eb6d8ce2
developer <developer@com>
2023-03-02 16:47:31 +0800

    Match-id-d2216c8ea8d20ebdf337b8197f9d09928acf21be

--- mef-center-install/scripts/upgrade.sh
+++ mef-center-install/scripts/upgrade.sh
@@ -6,10 +6,27 @@ declare -i ret_ok=0  # success
 CURRENT_PATH=$(dirname "$(readlink -f "$0")")
 cd "${CURRENT_PATH}" || exit 1
 UPGRADE_BIN_PATH="${CURRENT_PATH}/../bin"
+VERSION_XML_PATH="${CURRENT_PATH}/../version.xml"
 UPGRADE_BIN="MEF-center-upgrade"
+
+function check_arch()
+{
+    arch=$(grep '<ProcessorArchitecture>' "$VERSION_XML_PATH")
+    arch=${arch#*>}
+    arch=${arch%<*}
+    ret=$(uname -i)
+    if [ "$ret" != "$arch" ];then
+        return 1
+    fi
+    return 0
+}
 
 function main()
 {
+    if ! check_arch; then
+       echo "invalid arch" >&2
+       return 1
+    fi
     "${UPGRADE_BIN_PATH}"/"${UPGRADE_BIN}"
     ret=$?
     return ${ret}
