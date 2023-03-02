cea3e864fba3b64cd66fc61f6d4e66e66322885b
developer <developer@com>
2023-03-02 20:36:40 +0800

    Match-id-5f94f53f3d166b66eef154681bcaa84d1ca265ab

--- mef-center-install/scripts/upgrade.sh
+++ mef-center-install/scripts/upgrade.sh
@@ -2,6 +2,9 @@
 # Copyright (c) 2022. Huawei Technologies Co., Ltd. All rights reserved.
 umask 077
 declare -i ret_ok=0  # success
+declare -i ret_wrong_arch=2 # no arch ret
+declare -i ret_grep_wrong=3 # grep failed
+declare -i ret_uname_wrong=4 # uname -i failed
 
 CURRENT_PATH=$(dirname "$(readlink -f "$0")")
 cd "${CURRENT_PATH}" || exit 1
@@ -12,24 +15,40 @@ UPGRADE_BIN="MEF-center-upgrade"
 function check_arch()
 {
     arch=$(grep '<ProcessorArchitecture>' "$VERSION_XML_PATH")
+    if [ $? != 0 ]; then
+      return "${ret_grep_wrong}"
+    fi
     arch=${arch#*>}
     arch=${arch%<*}
     ret=$(uname -i)
-    if [ "$ret" != "$arch" ];then
-        return 1
+    if [ $? != 0 ]; then
+      return "${ret_uname_wrong}"
     fi
-    return 0
+    if [ "$ret" != "$arch" ];then
+        return "${ret_wrong_arch}"
+    fi
+    return "${ret_ok}"
 }
 
 function main()
 {
-    if ! check_arch; then
-       echo "invalid arch" >&2
-       return 1
-    fi
+    check_arch
+    ret=$?
+    case "${ret}" in
+        "${ret_wrong_arch}")
+           echo "invalid arch" >&2
+           return "${ret_wrong_arch}"
+        ;;
+        "${ret_grep_wrong}")
+           return "${ret_grep_wrong}"
+        ;;
+        "${ret_uname_wrong}")
+           return "${ret_uname_wrong}"
+        ;;
+    esac
     "${UPGRADE_BIN_PATH}"/"${UPGRADE_BIN}"
     ret=$?
-    return ${ret}
+    return "${ret}"
 }
 
 main "$@"
