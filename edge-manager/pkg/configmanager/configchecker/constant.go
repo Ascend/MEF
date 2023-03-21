72a6f5808b01127a2da0c3af90bbd977679f483c
developer <developer@com>
2023-03-21 14:59:37 +0800

    Match-id-5ab8d89d590c4008b4ae940a48bee918c8ca89cd

--- edge-manager/pkg/configmanager/configchecker/constant.go
+++ edge-manager/pkg/configmanager/configchecker/constant.go
@@ -9,5 +9,6 @@ const (
 	minPwdCount = 1
 	maxPwdCount = 20
 	nameReg     = "^[a-zA-Z0-9]([a-zA-Z0-9-_]{0,254}[a-zA-Z0-9]){0,1}$"
-	dnsReg      = "[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+\\.?"
+	dnsLenReg   = `^.{3,255}$`
+	dnsReg      = `^[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+$`
 )
