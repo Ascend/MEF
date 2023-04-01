47f21a6647ccf28b5db038e80b745eec90bf55e6
developer <developer@com>
2023-04-01 10:27:20 +0800

    Match-id-de18ced990319777491b30842b50d1b74cf9afd9

--- mef-center-install/pkg/util/config_mgr.go
+++ mef-center-install/pkg/util/config_mgr.go
@@ -14,12 +14,14 @@ import (
 // ConfigMgr is the struct to manager the mef-config dir
 type ConfigMgr struct {
 	cfgPathMgr *ConfigPathMgr
+	components []string
 }
 
 // GetConfigMgr is the func to init a ConfigMgr struct
-func GetConfigMgr(pathMgr *ConfigPathMgr) *ConfigMgr {
+func GetConfigMgr(pathMgr *ConfigPathMgr, components []string) *ConfigMgr {
 	return &ConfigMgr{
 		cfgPathMgr: pathMgr,
+		components: components,
 	}
 }
 
@@ -28,6 +30,7 @@ func (cm *ConfigMgr) DoPrepare() error {
 	var prepareConfigTasks = []func() error{
 		cm.prepareConfigDir,
 		cm.preparePubConfigDir,
+		cm.prepareComponentsConfig,
 	}
 
 	for _, function := range prepareConfigTasks {
@@ -62,6 +65,17 @@ func (cm *ConfigMgr) preparePubConfigDir() error {
 	if err = common.CopyDir(srcPath, dstPath, true); err != nil {
 		hwlog.RunLog.Errorf("prepare public-config dir failed: %s", err.Error())
 		return errors.New("prepare public-config dir failed")
+	}
+
+	return nil
+}
+
+func (cm *ConfigMgr) prepareComponentsConfig() error {
+	for _, component := range cm.components {
+		componentMgr := GetComponentMgr(component)
+		if err := componentMgr.PrepareComponentConfig(cm.cfgPathMgr); err != nil {
+			return err
+		}
 	}
 
 	return nil
