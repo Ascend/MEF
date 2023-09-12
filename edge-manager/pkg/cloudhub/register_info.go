c6ae4c5fe13b5aae8432c9d9d3131fc662d3669d
developer <developer@com>
2023-09-12 19:25:56 +0800

    Match-id-5ea4a4621d3cf2d09bc96ec46ff0b629ef283054

--- edge-manager/pkg/cloudhub/register_info.go
+++ edge-manager/pkg/cloudhub/register_info.go
@@ -12,6 +12,11 @@ import (
 	"huawei.com/mindxedge/base/common/requests"
 )
 
+const (
+	alarmHandlerRate     = 35
+	alarmHandlerCapacity = 90
+)
+
 var regInfoList = []websocketmgr.RegisterModuleInfo{
 	{MsgOpt: common.OptGet, MsgRes: common.ResConfig, ModuleName: common.NodeMsgManagerName},
 	{MsgOpt: common.OptReport, MsgRes: common.ResDownloadProgress, ModuleName: common.NodeMsgManagerName},
@@ -22,7 +27,8 @@ var regInfoList = []websocketmgr.RegisterModuleInfo{
 	{MsgOpt: common.OptGet, MsgRes: common.CertWillExpired, ModuleName: common.CloudHubName},
 	{MsgOpt: common.OptResp, MsgRes: common.CertWillExpired, ModuleName: common.CertUpdaterName},
 	{MsgOpt: common.Delete, MsgRes: common.DeleteNodeMsg, ModuleName: common.NodeManagerName},
-	{MsgOpt: common.OptPost, MsgRes: requests.ReportAlarmRouter, ModuleName: common.AlarmManagerName},
+	{MsgOpt: common.OptPost, MsgRes: requests.ReportAlarmRouter, ModuleName: common.AlarmManagerName,
+		MsgRate: alarmHandlerRate, MsgCapacity: alarmHandlerCapacity},
 	{MsgOpt: common.OptReport, MsgRes: constants.ResLogDumpError, ModuleName: constants.LogManagerName},
 }
 
