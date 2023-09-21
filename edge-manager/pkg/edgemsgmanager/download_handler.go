522af57e92bc92fc9ba33ee1a964d3d7c8d61ff5
developer <developer@com>
2023-09-21 22:25:45 +0800

    Match-id-edd6d58c80c45bcbce30f565f90245243c2e0734

--- edge-manager/pkg/edgemsgmanager/download_handler.go
+++ edge-manager/pkg/edgemsgmanager/download_handler.go
@@ -9,6 +9,7 @@ import (
 	"huawei.com/mindx/common/hwlog"
 	"huawei.com/mindx/common/modulemgr"
 	"huawei.com/mindx/common/modulemgr/model"
+	"huawei.com/mindx/common/utils"
 
 	"huawei.com/mindxedge/base/common"
 
@@ -23,51 +24,25 @@ func downloadSoftware(input interface{}) common.RespMsg {
 		hwlog.RunLog.Error("get message failed")
 		return common.RespMsg{Status: common.ErrorTypeAssert, Msg: "get message failed", Data: nil}
 	}
-
 	var req SoftwareDownloadInfo
-	var err error
-	if err = common.ParamConvert(message.GetContent(), &req); err != nil {
+	if err := common.ParamConvert(message.GetContent(), &req); err != nil {
 		return common.RespMsg{Status: common.ErrorParamConvert, Msg: err.Error(), Data: nil}
 	}
-
+	if req.DownloadInfo.Password != nil {
+		defer utils.ClearSliceByteMemory(*(req.DownloadInfo.Password))
+	}
 	if checkResult := NewDownloadChecker().Check(req); !checkResult.Result {
 		hwlog.RunLog.Errorf("check software download para failed: %s", checkResult.Reason)
 		return common.RespMsg{Status: common.ErrorParamInvalid, Msg: checkResult.Reason, Data: nil}
 	}
-
 	msg, err := model.NewMessage()
 	if err != nil {
 		hwlog.RunLog.Error("create message failed")
 		return common.RespMsg{Status: common.ErrorNewMsg, Msg: "create message failed", Data: nil}
 	}
-
 	msg.SetRouter(common.NodeMsgManagerName, common.CloudHubName, common.OptPost, common.ResEdgeDownloadInfo)
 	msg.FillContent(message.GetContent())
-	var batchResp types.BatchResp
-	failedMap := make(map[string]string)
-	batchResp.FailedInfos = failedMap
-	for _, sn := range req.SerialNumbers {
-		msg.SetNodeId(sn)
-
-		rsp, err := modulemgr.SendSyncMessage(msg, common.ResponseTimeout)
-		if err != nil {
-			errInfo := fmt.Sprintf("send software download info to %s failed", sn)
-			hwlog.RunLog.Error(errInfo)
-			failedMap[sn] = errInfo
-			continue
-		}
-
-		if content, ok := rsp.GetContent().(string); !ok || content != common.OK {
-			errInfo := fmt.Sprintf("mef edge process software download info in %s failed", sn)
-			hwlog.RunLog.Error(errInfo)
-			failedMap[sn] = errInfo
-			continue
-		}
-
-		batchResp.SuccessIDs = append(batchResp.SuccessIDs, sn)
-		nodesProgress[sn] = types.ProgressInfo{}
-	}
-
+	batchResp := sendDownloadInfo(req, msg)
 	if len(batchResp.FailedInfos) != 0 {
 		hwlog.RunLog.Error("deal edge software upgrade info failed")
 		return common.RespMsg{Status: common.ErrorSendMsgToNode, Msg: "", Data: batchResp}
@@ -75,4 +50,29 @@ func downloadSoftware(input interface{}) common.RespMsg {
 		hwlog.RunLog.Info("deal edge software download info success")
 		return common.RespMsg{Status: common.Success, Msg: "", Data: batchResp}
 	}
+}
+
+func sendDownloadInfo(req SoftwareDownloadInfo, msg *model.Message) types.BatchResp {
+	var batchResp types.BatchResp
+	failedMap := make(map[string]string)
+	batchResp.FailedInfos = failedMap
+	for _, sn := range req.SerialNumbers {
+		msg.SetNodeId(sn)
+		rsp, err := modulemgr.SendSyncMessage(msg, common.ResponseTimeout)
+		if err != nil {
+			errInfo := fmt.Sprintf("send software download info to %s failed", sn)
+			hwlog.RunLog.Error(errInfo)
+			failedMap[sn] = errInfo
+			continue
+		}
+		if content, ok := rsp.GetContent().(string); !ok || content != common.OK {
+			errInfo := fmt.Sprintf("mef edge process software download info in %s failed", sn)
+			hwlog.RunLog.Error(errInfo)
+			failedMap[sn] = errInfo
+			continue
+		}
+		batchResp.SuccessIDs = append(batchResp.SuccessIDs, sn)
+		nodesProgress[sn] = types.ProgressInfo{}
+	}
+	return batchResp
 }
