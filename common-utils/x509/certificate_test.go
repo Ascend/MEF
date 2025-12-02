41270dace52e406cd539ffc3616b8b7145279baa
developer <developer@com>
2025-12-02 09:12:04 +0800

    Match-id-aa245b799fc88f3fb36ff9eea572e1018ded0463

--- common-utils/x509/certificate_test.go
+++ common-utils/x509/certificate_test.go
@@ -1,4 +1,12 @@
-//  Copyright(c) 2022. Huawei Technologies Co.,Ltd.  All rights reserved.
+// Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+// MindEdge is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//          http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
 
 // Package x509 certificate test file
 package x509
@@ -25,6 +33,7 @@ import (
 
 	"github.com/agiledragon/gomonkey/v2"
 	"github.com/smartystreets/goconvey/convey"
+
 	"huawei.com/mindx/common/fileutils"
 
 	"huawei.com/mindx/common/hwlog"
