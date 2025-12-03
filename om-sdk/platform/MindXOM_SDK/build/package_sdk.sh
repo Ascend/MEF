c3add493f9c0cea7e357b6bc7e52d97296cb6e35
developer <developer@com>
2025-12-03 14:17:19 +0800

    Match-id-2e89163574bf44d573dd7e2c9e5f31af9ec5912d

--- om-sdk/platform/MindXOM_SDK/build/package_sdk.sh
+++ om-sdk/platform/MindXOM_SDK/build/package_sdk.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-# Copyright © Huawei Technologies Co., Ltd. 2020-2025. All rights reserved.
+# Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+# MindEdge is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
 CUR_DIR=$(dirname $(readlink -f "$0"))
 TOP_DIR="${CUR_DIR}"/..
 OUTPUT_PACKAGE_DIR="${TOP_DIR}"/output/package
