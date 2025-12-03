a233e7f6e6891a6e0499e127fa5e0f8c75c3ea7f
developer <developer@com>
2025-12-03 10:49:59 +0800

    Match-id-c65dedd0227360a23bb89e8db7fb4ccf15651613

--- mef-center/edge-manager/pkg/logmanager/handlers/report_error_handler.go
+++ mef-center/edge-manager/pkg/logmanager/handlers/report_error_handler.go
@@ -1,4 +1,12 @@
-// Copyright (c)  2023. Huawei Technologies Co., Ltd.  All rights reserved.
+// Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+// MindEdge is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//          http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
 
 // Package handlers provides handlers to process business logic of log collection
 package handlers
@@ -10,6 +18,7 @@ import (
 	"huawei.com/mindx/common/hwlog"
 	"huawei.com/mindx/common/modulemgr/handler"
 	"huawei.com/mindx/common/modulemgr/model"
+
 	"huawei.com/mindxedge/base/common"
 	"huawei.com/mindxedge/base/common/taskschedule"
 
