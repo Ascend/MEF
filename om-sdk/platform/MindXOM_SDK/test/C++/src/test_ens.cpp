50d6b8904b4ca1fb78774aad2905c335d8cf47a8
developer <developer@com>
2025-12-03 17:34:24 +0800

    Match-id-0c71509819e620ee342cfd83820a207e0cdc65fe

--- om-sdk/platform/MindXOM_SDK/test/C++/src/test_ens.cpp
+++ om-sdk/platform/MindXOM_SDK/test/C++/src/test_ens.cpp
@@ -1,8 +1,14 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2021-2021. All rights reserved.
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+   MindEdge is licensed under Mulan PSL v2.
+   You can use this software according to the terms and conditions of the Mulan PSL v2.
+   You may obtain a copy of Mulan PSL v2 at:
+            http://license.coscl.org.cn/MulanPSL2
+   THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+   EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+   MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+   See the Mulan PSL v2 for more details.
  * Description: test ensd
- * Author: dingbanglin wx1009413
- * Create: 2021-11-11
  */
 
 #include <stdio.h>
