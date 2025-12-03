f8ec71c0248673da53c1bca1ae56a1ed2b4bdc15
developer <developer@com>
2025-12-03 09:50:26 +0800

    Match-id-c41414e3103254fcad62ec330dce934667c1aaba

--- mef-center/build/common_utils_dt_exclude.sh
+++ mef-center/build/common_utils_dt_exclude.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# Copyright (c) 2024. Huawei Technologies Co., Ltd. All rights reserved.
+# Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+# MindEdge is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
 # Description: 构建后去除DT使用的开源软件依赖脚本
 set -e
 BUILD_DEPENDENCY_CACHE_DIR="/tmp/.next3rd/"
