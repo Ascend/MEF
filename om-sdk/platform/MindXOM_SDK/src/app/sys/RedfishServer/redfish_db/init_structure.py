681f4fff533ab9b2f238a916bb8eae6d56020ca8
developer <developer@com>
2025-12-03 15:23:43 +0800

    Match-id-6a8ac4150638ec488b2a30dd3a488ae8eb137d4c

--- om-sdk/platform/MindXOM_SDK/src/app/sys/RedfishServer/redfish_db/init_structure.py
+++ om-sdk/platform/MindXOM_SDK/src/app/sys/RedfishServer/redfish_db/init_structure.py
@@ -1,5 +1,13 @@
 # -*- coding: UTF-8 -*-
-# Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
+# Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+# MindEdge is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
 """
 首建模型字段持久化(需要平滑的表才记录)
     当首次新增了一个sqlite数据库ORM模型，必须将模型对应的字段名写入到此文件中的INIT_COLUMNS中；
