dfe1c8635d1733f59311a59644d284a3a8a7f9ad
yangjingyao <yangjingyao@huawei.com>
2026-05-11 15:43:06 +0800

    修复测试用例并发问题
    Co-authored-by: yangjingyao<yangjingyao@huawei.com>

--- src/mef-edge/edge-installer/pkg/edge-main/handlermgr/handlers/dump_log_handler_test.go
+++ src/mef-edge/edge-installer/pkg/edge-main/handlermgr/handlers/dump_log_handler_test.go
@@ -54,90 +54,95 @@ const testDumpLogMsg = `{
 }`
 
 var (
-	dumpLogMsg     model.Message
 	dumpLogContent = DumpLogReq{
 		Module: "edgeNode",
 		TaskId: "dumpSingleNodeLog.2102312NSF10K8000130.859a88487aac6f2f12d4e094",
 	}
-	process = dumpLogProcess{handler: getDumpLogHandler(), taskId: dumpLogContent.TaskId}
 )
 
-func setupDumpLogHandler() error {
+func createDumpLogMsg() (model.Message, error) {
+	var dumpLogMsg model.Message
 	if err := json.Unmarshal([]byte(testDumpLogMsg), &dumpLogMsg); err != nil {
 		hwlog.RunLog.Errorf("unmarshal test dump log message failed, error: %v", err)
-		return err
+		return dumpLogMsg, err
 	}
 	if err := dumpLogMsg.FillContent(dumpLogContent); err != nil {
 		hwlog.RunLog.Errorf("fill test dump log content failed, error: %v", err)
-		return err
+		return dumpLogMsg, err
 	}
-	return nil
+	return dumpLogMsg, nil
 }
 
 func TestDumpLogHandler(t *testing.T) {
-	if err := setupDumpLogHandler(); err != nil {
-		panic(err)
-	}
-
-	p := gomonkey.ApplyPrivateMethod(&dumpLogProcess{}, "process", func(*dumpLogProcess) error { return nil })
-	defer p.Reset()
-
-	convey.Convey("test dump log handler should be success", t, dumpLogHandlerSuccess)
 	convey.Convey("test dump log handler should be failed, parse and check args failed", t, parseAndCheckArgsFailed)
 	convey.Convey("test dump log handler should be failed, dump log handler busy", t, dumpLogHandlerBusyFailed)
 }
 
-func dumpLogHandlerSuccess() {
-	p1 := gomonkey.ApplyFuncReturn(modulemgr.SendMessage, nil)
-	defer p1.Reset()
-	err := getDumpLogHandler().Handle(&dumpLogMsg)
-	convey.So(err, convey.ShouldBeNil)
-}
-
 func parseAndCheckArgsFailed() {
-	invalidDumpLogMsg := dumpLogMsg
+	handler := &dumpLogHandler{}
 	convey.Convey("parse content failed", func() {
 		p1 := gomonkey.ApplyMethodReturn(&model.Message{}, "ParseContent", test.ErrTest)
 		defer p1.Reset()
-		err := getDumpLogHandler().Handle(&dumpLogMsg)
+		dumpLogMsg, err := createDumpLogMsg()
+		if err != nil {
+			panic(err)
+		}
+		err = handler.Handle(&dumpLogMsg)
 		convey.So(err, convey.ShouldResemble, fmt.Errorf("failed to parse or check args,"+
 			" parma convert error: %v", test.ErrTest))
 	})
 
 	convey.Convey("invalid Module argument", func() {
-		err := invalidDumpLogMsg.FillContent(DumpLogReq{Module: ""})
+		invalidDumpLogMsg, err := createDumpLogMsg()
+		if err != nil {
+			panic(err)
+		}
+		err = invalidDumpLogMsg.FillContent(DumpLogReq{Module: ""})
 		convey.So(err, convey.ShouldBeNil)
 
-		err = getDumpLogHandler().Handle(&invalidDumpLogMsg)
+		err = handler.Handle(&invalidDumpLogMsg)
 		convey.So(err, convey.ShouldResemble, errors.New("failed to parse or check args, invalid argument"))
 	})
 
 	convey.Convey("invalid TaskId argument", func() {
-		err := invalidDumpLogMsg.FillContent(DumpLogReq{
+		invalidDumpLogMsg, err := createDumpLogMsg()
+		if err != nil {
+			panic(err)
+		}
+		err = invalidDumpLogMsg.FillContent(DumpLogReq{
 			Module: "edgeNode",
 			TaskId: "2102312NSF10K8000130.859a88487aac6f2f12d4e094",
 		})
 		convey.So(err, convey.ShouldBeNil)
 
-		err = getDumpLogHandler().Handle(&invalidDumpLogMsg)
+		err = handler.Handle(&invalidDumpLogMsg)
 		convey.So(err, convey.ShouldResemble, errors.New("failed to parse or check args, invalid argument"))
 	})
 }
 
 func dumpLogHandlerBusyFailed() {
-	handler := getDumpLogHandler()
-	handler.running = 1
+	handler := &dumpLogHandler{
+		running: 1,
+	}
 	convey.Convey("dump log handler busy, feedback error success", func() {
 		p1 := gomonkey.ApplyFuncReturn(modulemgr.SendMessage, nil)
 		defer p1.Reset()
-		err := handler.Handle(&dumpLogMsg)
+		dumpLogMsg, err := createDumpLogMsg()
+		if err != nil {
+			panic(err)
+		}
+		err = handler.Handle(&dumpLogMsg)
 		convey.So(err, convey.ShouldResemble, errors.New("dump log handler busy"))
 	})
 
 	convey.Convey("dump log handler busy, feedback error failed", func() {
 		p1 := gomonkey.ApplyFuncReturn(modulemgr.SendMessage, test.ErrTest)
 		defer p1.Reset()
-		err := handler.Handle(&dumpLogMsg)
+		dumpLogMsg, err := createDumpLogMsg()
+		if err != nil {
+			panic(err)
+		}
+		err = handler.Handle(&dumpLogMsg)
 		convey.So(err, convey.ShouldResemble, errors.New("dump log handler busy"))
 	})
 }
@@ -146,7 +151,8 @@ func TestDumpLogProcess(t *testing.T) {
 	p := gomonkey.ApplyFuncReturn(configpara.GetNetConfig, config.NetManager{NetType: "MEF", IP: "127.0.0.1"}).
 		ApplyPrivateMethod(&dumpLogProcess{}, "packLogs", func(*dumpLogProcess) error { return nil }).
 		ApplyFuncReturn(cloudcert.GetEdgeHubCertInfo, &certutils.TlsCertInfo{}, nil).
-		ApplyPrivateMethod(&dumpLogProcess{}, "uploadLogs", func(*dumpLogProcess) error { return nil })
+		ApplyPrivateMethod(&dumpLogProcess{}, "uploadLogs", func(*dumpLogProcess) error { return nil }).
+		ApplyFuncReturn(modulemgr.SendMessage, nil)
 	defer p.Reset()
 
 	convey.Convey("test dump log process should be success", t, dumpLogProcessSuccess)
@@ -158,6 +164,7 @@ func TestDumpLogProcess(t *testing.T) {
 }
 
 func dumpLogProcessSuccess() {
+	process := dumpLogProcess{handler: getDumpLogHandler(), taskId: dumpLogContent.TaskId}
 	err := process.process()
 	convey.So(err, convey.ShouldBeNil)
 }
@@ -165,6 +172,7 @@ func dumpLogProcessSuccess() {
 func getNetConfigFailed() {
 	p1 := gomonkey.ApplyFuncReturn(configpara.GetNetConfig, config.NetManager{})
 	defer p1.Reset()
+	process := dumpLogProcess{handler: getDumpLogHandler(), taskId: dumpLogContent.TaskId}
 	err := process.process()
 	convey.So(err, convey.ShouldResemble, errors.New("failed to get net config, ip is invalid"))
 }
@@ -172,6 +180,7 @@ func getNetConfigFailed() {
 func getCertInfoFailed() {
 	p1 := gomonkey.ApplyFuncReturn(cloudcert.GetEdgeHubCertInfo, &certutils.TlsCertInfo{}, test.ErrTest)
 	defer p1.Reset()
+	process := dumpLogProcess{handler: getDumpLogHandler(), taskId: dumpLogContent.TaskId}
 	err := process.process()
 	convey.So(err, convey.ShouldResemble, errors.New("failed to get tls cert info"))
 }
@@ -179,6 +188,7 @@ func getCertInfoFailed() {
 func packLogsFailed() {
 	p1 := gomonkey.ApplyPrivateMethod(&dumpLogProcess{}, "packLogs", func(*dumpLogProcess) error { return test.ErrTest })
 	defer p1.Reset()
+	process := dumpLogProcess{handler: getDumpLogHandler(), taskId: dumpLogContent.TaskId}
 	err := process.process()
 	convey.So(err, convey.ShouldResemble, errors.New("failed to pack logs"))
 }
@@ -186,6 +196,7 @@ func packLogsFailed() {
 func uploadLogsFailed() {
 	p1 := gomonkey.ApplyPrivateMethod(&dumpLogProcess{}, "uploadLogs", func(*dumpLogProcess) error { return test.ErrTest })
 	defer p1.Reset()
+	process := dumpLogProcess{handler: getDumpLogHandler(), taskId: dumpLogContent.TaskId}
 	err := process.process()
 	convey.So(err, convey.ShouldResemble, errors.New("failed to upload logs"))
 }
@@ -194,6 +205,7 @@ func TestPackLogs(t *testing.T) {
 	convey.Convey("failed to create request message", t, func() {
 		p := gomonkey.ApplyFuncReturn(model.NewMessage, nil, test.ErrTest)
 		defer p.Reset()
+		process := dumpLogProcess{handler: getDumpLogHandler(), taskId: dumpLogContent.TaskId}
 		err := process.packLogs()
 		convey.So(err, convey.ShouldResemble, fmt.Errorf("failed to create request message, %v", test.ErrTest))
 	})
@@ -201,6 +213,7 @@ func TestPackLogs(t *testing.T) {
 	convey.Convey("failed to send request", t, func() {
 		p := gomonkey.ApplyFuncReturn(modulemgr.SendMessage, test.ErrTest)
 		defer p.Reset()
+		process := dumpLogProcess{handler: getDumpLogHandler(), taskId: dumpLogContent.TaskId}
 		err := process.packLogs()
 		convey.So(err, convey.ShouldResemble, fmt.Errorf("failed to send request, %v", test.ErrTest))
 	})
@@ -231,7 +244,8 @@ func TestUploadLogs(t *testing.T) {
 	}()
 
 	p := gomonkey.ApplyGlobalVar(&localFilePath, testLocalFilePath).
-		ApplyMethodReturn(&httpsmgr.HttpsRequest{}, "PostFile", []byte(constants.OK), nil)
+		ApplyMethodReturn(&httpsmgr.HttpsRequest{}, "PostFile", []byte(constants.OK), nil).
+		ApplyFuncReturn(modulemgr.SendMessage, nil)
 	defer p.Reset()
 
 	convey.Convey("upload logs should be success", t, uploadLogsSuccess)
@@ -243,6 +257,11 @@ func TestUploadLogs(t *testing.T) {
 }
 
 func uploadLogsSuccess() {
+	process := dumpLogProcess{
+		handler:     getDumpLogHandler(),
+		taskId:      dumpLogContent.TaskId,
+		tlsCertInfo: &certutils.TlsCertInfo{},
+	}
 	err := process.uploadLogs()
 	convey.So(err, convey.ShouldBeNil)
 }
@@ -250,6 +269,7 @@ func uploadLogsSuccess() {
 func openLocalFileFailed() {
 	p1 := gomonkey.ApplyFuncReturn(os.Open, nil, test.ErrTest)
 	defer p1.Reset()
+	process := dumpLogProcess{handler: getDumpLogHandler(), taskId: dumpLogContent.TaskId}
 	err := process.uploadLogs()
 	convey.So(err, convey.ShouldResemble, fmt.Errorf("failed to open local file, %v", test.ErrTest))
 }
@@ -257,6 +277,7 @@ func openLocalFileFailed() {
 func checkLocalFileFailed() {
 	p1 := gomonkey.ApplyMethodReturn(&fileutils.FileLinkChecker{}, "Check", test.ErrTest)
 	defer p1.Reset()
+	process := dumpLogProcess{handler: getDumpLogHandler(), taskId: dumpLogContent.TaskId}
 	err := process.uploadLogs()
 	convey.So(err, convey.ShouldResemble, fmt.Errorf("failed to check temp file, %v", test.ErrTest))
 }
@@ -264,6 +285,7 @@ func checkLocalFileFailed() {
 func calculateChecksumFailed() {
 	p1 := gomonkey.ApplyFuncReturn(io.Copy, int64(0), test.ErrTest)
 	defer p1.Reset()
+	process := dumpLogProcess{handler: getDumpLogHandler(), taskId: dumpLogContent.TaskId}
 	err := process.uploadLogs()
 	convey.So(err, convey.ShouldResemble, fmt.Errorf("failed to calculate checksum, %v", test.ErrTest))
 }
@@ -271,6 +293,11 @@ func calculateChecksumFailed() {
 func uploadFileFailed() {
 	p1 := gomonkey.ApplyMethodReturn(&httpsmgr.HttpsRequest{}, "PostFile", nil, test.ErrTest)
 	defer p1.Reset()
+	process := dumpLogProcess{
+		handler:     getDumpLogHandler(),
+		taskId:      dumpLogContent.TaskId,
+		tlsCertInfo: &certutils.TlsCertInfo{},
+	}
 	err := process.uploadLogs()
 	convey.So(err, convey.ShouldResemble, fmt.Errorf("failed to upload file, %v", test.ErrTest))
 }
@@ -278,6 +305,11 @@ func uploadFileFailed() {
 func unexpectedResponse() {
 	p1 := gomonkey.ApplyMethodReturn(&httpsmgr.HttpsRequest{}, "PostFile", []byte(constants.Failed), nil)
 	defer p1.Reset()
+	process := dumpLogProcess{
+		handler:     getDumpLogHandler(),
+		taskId:      dumpLogContent.TaskId,
+		tlsCertInfo: &certutils.TlsCertInfo{},
+	}
 	err := process.uploadLogs()
 	convey.So(err, convey.ShouldResemble, fmt.Errorf("unexpected response from center: %s", []byte(constants.Failed)))
 }
